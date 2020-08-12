8f6b39378732dff182408374e81ef5dcf0bbf6d9
unknown <soumen.rim@gmail.com>
2020-08-12 17:21:37 +0530

    Update scroll with toggle menu

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,5 @@
 // JavaScript Document
 
-
    $(document).ready(function(){
        var a = false;
        $("#menu").click(function(){
@@ -14,6 +13,37 @@
            }
            
        });
+
+    // $(".menus li").click(function () {
+    //     $('.menus li > ul').not($(this).children("ul").toggle()).hide();
+        
+    // });
+
+    $(".menus li").click(function () {
+        $('.menus li').removeClass('active')
+        $(this).addClass('active')
+    });
+
+       function reportSize() {
+            var browserHeight = $( window ).outerHeight();
+            var headerHeight = $('#header').outerHeight();
+            var footerHeight = $('#footer').outerHeight();
+            var buttonFooterHeight = $('#bottomFooter').outerHeight();
+            var clientEntryHeader = $('#clientEntryHeader').outerHeight();
+
+            var contactTable = $('.tableContent').outerHeight();
+            var contactFormHeader = $('.contact_information_head').outerHeight();
+            // console.log('On load', browserHeight, headerHeight, footerHeight, buttonFooterHeight, clientEntryHeader);
+            var clientEntryScrollHeight = browserHeight - headerHeight - footerHeight - buttonFooterHeight - clientEntryHeader - 25;
+            var contactEntryScrollHeight = browserHeight - headerHeight - footerHeight - buttonFooterHeight - contactTable - contactFormHeader - 70;
+            $(".clientEntryScroll").css("height", clientEntryScrollHeight);
+            $(".contactEntryScroll").css("height", contactEntryScrollHeight);
+            
+        }
+        reportSize()
+        $(window).resize(function(){
+            reportSize()
+        });
    });
 
 
