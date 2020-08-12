bc693e92e30d492d3739abb8f791be2757f85b42
unknown <soumen.rim@gmail.com>
2020-08-13 02:56:12 +0530

    Update internal style and script

--- js/custom.js
+++ js/custom.js
@@ -1,70 +1,66 @@
 // JavaScript Document
 
-   $(document).ready(function(){
-       var a = false;
-       $("#menu").click(function(){
-           if(a == false){
-            $(".left_side_bar").addClass("show");
-               a = true;
-           }
-           else{
-               $(".left_side_bar").removeClass("show");
-               a = false;
-           }
-           
-       });
-
-    // $(".menus li").click(function () {
-    //     $('.menus li > ul').not($(this).children("ul").toggle()).hide();
+$(document).ready(function(){
+    var a = false;
+    $("#menu").click(function(){
+        if(a == false){
+        $(".left_side_bar").addClass("show");
+            a = true;
+        }
+        else{
+            $(".left_side_bar").removeClass("show");
+            a = false;
+        }
         
-    // });
+    });
 
     $(".menus li").click(function () {
         $('.menus li').removeClass('active')
         $(this).addClass('active')
     });
 
-       function reportSize() {
-            var browserWidth = $( window ).outerWidth();
-            var browserHeight = $( window ).outerHeight();
-            var headerHeight = $('#header').outerHeight();
-            var footerHeight = $('#footer').outerHeight();
-            var buttonFooterHeight = $('#bottomFooter').outerHeight();
-            var clientEntryHeader = $('#clientEntryHeader').outerHeight();
+    function reportSize() {
+        var browserWidth = $( window ).outerWidth();
+        var browserHeight = $( window ).outerHeight();
+        var headerHeight = $('#header').outerHeight();
+        var footerHeight = $('#footer').outerHeight();
+        var buttonFooterHeight = $('#bottomFooter').outerHeight();
+        var clientEntryHeader = $('#clientEntryHeader').outerHeight();
 
-            var contactTable = $('.tableContent').outerHeight();
-            var contactFormHeader = $('.contact_information_head').outerHeight();
-            // console.log('On load', browserWidth, browserHeight, headerHeight, footerHeight, buttonFooterHeight, clientEntryHeader);
-            var clientEntryScrollHeight = browserHeight - headerHeight - footerHeight - buttonFooterHeight - clientEntryHeader - 25;
-            var contactEntryScrollHeight = browserHeight - headerHeight - footerHeight - buttonFooterHeight - contactTable - contactFormHeader - 70;
-            var leftSideBarHeight = browserHeight - headerHeight - footerHeight - 25;
-            var contactTableBodyHeight = Math.round((browserHeight - headerHeight - footerHeight - buttonFooterHeight - 60) / 2) ;
-                        
-            if(browserWidth >= 768){
-                $(".clientEntryScroll").css("height", clientEntryScrollHeight);
-                $(".contactEntryScroll").css("height", contactEntryScrollHeight);
-                $(".contact_table tbody").css("height", contactTableBodyHeight);
-            }else {
-                $(".clientEntryScroll").css("height", 'auto');
-                $(".contactEntryScroll").css("height", 'auto');
-                $(".contact_table tbody").css("height", 'auto');
-            }
-
-            if(browserHeight <= 768){
-                $(".left_side_bar").css("height", leftSideBarHeight);
-            }else {
-                $(".left_side_bar").css("height", 'auto');
-            }
-            
-            
+        var contactTable = $('.tableContent').outerHeight();
+        var contactFormHeader = $('.contact_information_head').outerHeight();
+        // console.log('On load', browserWidth, browserHeight, headerHeight, footerHeight, buttonFooterHeight, clientEntryHeader);
+        var clientEntryScrollHeight = browserHeight - headerHeight - footerHeight - buttonFooterHeight - clientEntryHeader - 25;
+        var contactEntryScrollHeight = browserHeight - headerHeight - footerHeight - buttonFooterHeight - contactTable - contactFormHeader - 70;
+        var leftSideBarHeight = browserHeight - headerHeight - footerHeight - 25;
+        var contactTableBodyHeight = Math.round((browserHeight - headerHeight - footerHeight - buttonFooterHeight - 60) / 2) ;
+                    
+        if(browserWidth >= 768){
+            $(".clientEntryScroll").css("height", clientEntryScrollHeight);
+            $(".contactEntryScroll").css("height", contactEntryScrollHeight);
+            $(".contact_table tbody").css("height", contactTableBodyHeight);
+        }else {
+            $(".clientEntryScroll").css("height", 'auto');
+            $(".contactEntryScroll").css("height", 'auto');
+            $(".contact_table tbody").css("height", 'auto');
         }
-        reportSize()
-        $(window).resize(function(){
+
+        if(browserHeight <= 768){
+            $(".left_side_bar").css("height", leftSideBarHeight);
+        }else {
+            $(".left_side_bar").css("height", 'auto');
+        }
+    }
+    reportSize()
+    // $(window).resize(function(){
+    //     reportSize()
+    // });
+    $(window).resize(function() {
+        if(screen.width == window.innerWidth || screen.width > window.innerWidth || screen.width < window.innerWidth){
             reportSize()
+        }
         });
-   });
-
-
+});
 $(window).scroll(function(){
   var sticky = $('.header_main'),
       scroll = $(window).scrollTop();
